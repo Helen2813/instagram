c8c338a8b19512f6ceb6e9e543a7c77d8c5fc3b0
Elena Krikun <elena.krikun@quantori.com>
2022-12-16 15:45:09 +0400

    add search bar to the header component

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Image from "next/image";
+import { SearchIcon } from "@heroicons/react/outline"
 
 function Header() {
   return (
-    <div className="">
-      <div className="flex items-center justify-between max--w-6xl">
-        <div className="h-24 w-29 relative hidden cursor-pointer lg:inline-grid">
+    <div className="flex items-center justify-between max-w-6xl">
+        <div className="h-20 w-28 relative hidden cursor-pointer lg:inline-grid">
           <Image
             width="200"
             height="100"
             src="https://globalsew.com/wp-content/uploads/2021/12/59-590993_follow-us-on-instagram-logo-png-clipart-1024x365.png"
-            className="object-contain"
+            className="object-contain mt-4"
             alt="logo"
           />
         </div>
@@ -23,9 +23,15 @@ function Header() {
             alt="logo"
           />
         </div>
+      <div className="relative mt-1">
+        <div className="absolute top-2 left-2">
+          <SearchIcon className="h-5 text-gray-500" />
+        </div>
+        <input
+          className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+          type="text" placeholder="Search"
+        />
       </div>
-
-
     </div>
   );
 }
