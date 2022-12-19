c36bd9fb30b91c6b5441176d0912ec130b6cf088
Elena Krikun <elena.krikun@quantori.com>
2022-12-19 21:20:01 +0400

    make the Feed section responsive

--- components/Feed.js
+++ components/Feed.js
@@ -4,10 +4,13 @@ import Posts from "./Posts";
 
 function Feed() {
   return (
-    <main className="">
-      <section>
+    <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto">
+      <section className="md:col-span-2">
         <Stories />
         <Posts />
+      </section>
+      <section className="hidden md:inline-grid md:col-span-1">
+
       </section>
     </main>
   );
