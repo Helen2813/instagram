c45544b5800ce748178f2dd9f78e65bfc496be9e
Elena Krikun <elena.krikun@quantori.com>
2022-12-19 21:57:18 +0400

    bild the suggestion part of feed component

--- components/Feed.js
+++ components/Feed.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Stories from "./Stories";
 import Posts from "./Posts";
 import MiniProfile from "./MiniProfile";
+import Suggestions from "./Suggestions";
 
 function Feed() {
   return (
@@ -13,6 +14,7 @@ function Feed() {
       <section className="hidden md:inline-grid md:col-span-1">
         <div className="fixed w-[380px]">
           <MiniProfile />
+          <Suggestions />
         </div>
       </section>
     </main>
