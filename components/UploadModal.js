a6af704c2080445001539489c8bc76a45352fc47
Elena Krikun <elena.krikun@quantori.com>
2022-12-23 12:40:55 +0400

    init Modal component

--- components/UploadModal.js
+++ components/UploadModal.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { modalState } from "../atom/modalAtom";
 import { useRecoilState } from "recoil";
+import Modal from "react-modal";
 
 function UploadModal() {
   const [isOpen, setIsOpen] = useRecoilState(modalState);
@@ -8,7 +9,16 @@ function UploadModal() {
   return (
     <div>
       {isOpen && (
-        <h1>open</h1>
+        <Modal
+          className="max-w-lg w-[90%] h-[300px] absolute top-56 left-[50%] translate-x-[-50%] bg-white
+          border-2 rounded-md shadow-md"
+          isOpen={isOpen}
+          onRequestClose={() => setIsOpen(false)}
+        >
+          <div className="flex flex-col justify-center items-center h-[100%]">
+            <h1>Modal</h1>
+          </div>
+        </Modal>
       )}
     </div>
   );
